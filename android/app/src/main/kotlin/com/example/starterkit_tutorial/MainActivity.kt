a61962bbfe6de0703c13e5d125e34c0fb62a095e
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2020-12-07 01:16:29 +0700

    tutorial-#2

---
+++ android/app/src/main/kotlin/com/example/starterkit_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.starterkit_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ packages/reskara_skit/lib/reskara_skit.dart
@@ -0,0 +1,7 @@
+library reskara_skit;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
